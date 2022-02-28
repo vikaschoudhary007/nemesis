44f3d5e19bbdf6494469aa026e18acbaaf5dd6b1
vikaschoudhary007 <vikas choudhary572001@gmail.com>
2022-02-28 10:18:48 +0530

    changing envs

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin:"*",
+  methods:['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}))
 
 // connecting mongodb ******* change the mongodb connection ID in the .env file **********************
 mongoose.connect(process.env.MDB_CONNECT, 
